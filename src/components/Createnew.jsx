30f8d50ff0923fb9c4a688a9309495da7cc93fc9
Anurag Kundu <154730001+kunduanurag064@users.noreply.github.com>
2024-07-03 22:17:37 +0530

    Update Createnew.jsx

--- src/components/Createnew.jsx
+++ src/components/Createnew.jsx
@@ -57,7 +57,7 @@ const Createnew = ()=>{
         setTitle("");
 
         try {
-            const response = await fetch('http://web-server-nu-two.vercel.app//api/user/createpost',{
+            const response = await fetch('http://web-server-nu-two.vercel.app/api/user/createpost',{
                 method:'POST',
                 headers:{
                     'Content-Type':'Application/json'
